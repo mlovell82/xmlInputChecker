21adadcdd4a7beff54faf58fa9f14bd21de92c8b
Mick Lovell <mlovell@kent.edu>
2022-04-05 16:03:08 -0400

    Create xmlParser.py

---
+++ xmlParser.py
@@ -0,0 +1,29 @@
+"""
+Here are possible classes for the project
+  Table
+  Activity
+  Category
+  Cluster
+  Condition
+  Contagion
+  Crew
+  Database
+  DefaultField
+  EnumField
+  Field
+  FieldRange
+  RangeField
+  RangeFields
+  Resource
+  StringEnumField
+  StringField
+  Suscept
+  Treatment
+  Validation
+  Workbook
+Here are some possible module scripts
+  ImportExport
+  Resize
+  
+"""
+print("test")
