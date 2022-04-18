dc6592f96c5ca6f8887ff9406e79c108700f02e3
Mick Lovell <mlovell@kent.edu>
2022-04-18 07:24:18 -0400

    Update xmlParser.py

--- xmlParser.py
+++ xmlParser.py
@@ -1,6 +1,32 @@
+from mimetypes import init
+import xml.etree.ElementTree as ET
+from crew_class import *
+from node_class import *
+
+'''import sys
+with open(sys.argv[1], 'r') as f:
+    tree3 = ET.parse(f)'''
+
+#Future topics to research
+#2. Obtaining the xml file from the command line
+#4. Calling the input checker from a c++ project with xml files
+
+tree = ET.parse('crew.xml')
+root = tree.getroot()
+#print(root)
+tree2 = ET.parse('segment.xml')
+root2 = tree2.getroot()
+crewBin = []
+for item in root.findall('crew'):
+    crew2 = Crew(item)
+    crew2.validateNodes()
+    crewBin.append(crew2)
+
+
+
+
 """
 Here are possible classes for the project
-  Table
   Activity
   Category
   Cluster
@@ -12,6 +38,7 @@ Here are possible classes for the project
   EnumField
   Field
   FieldRange
+  Node
   RangeField
   RangeFields
   Resource
