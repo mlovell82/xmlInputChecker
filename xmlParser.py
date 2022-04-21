f45d2e352337dcbe9cf389ff04ae5b9b31f55383
Mick Lovell <mlovell@kent.edu>
2022-04-21 16:07:31 -0400

    Update xmlParser.py

--- xmlParser.py
+++ xmlParser.py
@@ -1,7 +1,8 @@
 from mimetypes import init
 import xml.etree.ElementTree as ET
-from crew_class import *
-from node_class import *
+from crew import *
+from segment import *
+from node import *
 
 '''import sys
 with open(sys.argv[1], 'r') as f:
@@ -9,48 +10,21 @@ with open(sys.argv[1], 'r') as f:
 
 #Future topics to research
 #2. Obtaining the xml file from the command line
-#4. Calling the input checker from a c++ project with xml files
 
 tree = ET.parse('crew.xml')
-root = tree.getroot()
-#print(root)
 tree2 = ET.parse('segment.xml')
+root = tree.getroot()
 root2 = tree2.getroot()
 crewBin = []
+segmentBin = []
 for item in root.findall('crew'):
-    crew2 = Crew(item)
-    crew2.validateNodes()
-    crewBin.append(crew2)
+    crew = Crew(item)
+    print("Validation for Crew:")
+    crew.validateNodes()
+    crewBin.append(crew)
 
-
-
-
-"""
-Here are possible classes for the project
-  Activity
-  Category
-  Cluster
-  Condition
-  Contagion
-  Crew
-  Database
-  DefaultField
-  EnumField
-  Field
-  FieldRange
-  Node
-  RangeField
-  RangeFields
-  Resource
-  StringEnumField
-  StringField
-  Suscept
-  Treatment
-  Validation
-  Workbook
-Here are some possible module scripts
-  ImportExport
-  Resize
-  
-"""
-print("test")
+for item in root2.findall('segment'):
+    segment = Segment(item)
+    print("Validation for Segments:")
+    segment.validateNodes()
+    segmentBin.append(segment)
